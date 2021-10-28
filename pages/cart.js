6289b5178e26b3eadd6fe74e6b62d9c027b8a922
dieptv <diep.tv1999@gmail.com>
2021-10-28 17:57:05 +0700

    init

--- pages/cart.js
+++ pages/cart.js
@@ -10,10 +10,9 @@ function Cart({}) {
   return (
     <div className="min-h-screen flex justify-center mb-40 m-head">
       <div className="max-w-screen-lg w-full h-full flex flex-col pb-6">
-        <MenuCustom />
+        <MenuCustom/>
         <div className="m-4 mt-8 text-xl font-semibold self-start">My Shopping Cart</div>
-        <ButtonCustom className="w-72 flex items-center justify-center text-lg font-semibold py-3"
-                      onClick={router.push(constant.routes.CHECKOUT)}
+        <ButtonCustom className="w-72 flex items-center justify-center text-lg font-semibold py-3" onClick={router.push(constant.routes.CHECKOUT)}
         ><FaChevronRight className="mr-2"/>Proceed to Checkout</ButtonCustom>
       </div>
     </div>
